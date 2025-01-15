32aff3c05e2d4c3ba141d78d7db0b53c71e0d794
ravikumar8082 <ravikumargsm8082@gmail.com>
2025-01-15 23:25:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function sendMail(){
 let parms = {
     name : document.getElementById("fullName").value,
     cnic : document-getElementById("cnic").value,
-    email : document-getElementById("email").value,
     lastDegree : document.getElementById("lastDegree").value,
-    contact : document.getElementById("contact").value,
+    email : document-getElementById("email").value,
+    contact : document.getElementById("contact").value
 }
   emailjs.send("service_iioq66r","template_ljeu13d",parms).then(alert("Email Sent!!"))
 }
